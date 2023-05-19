b6bfe87d48b6447ba89a2c5a946b13898a1d5eb2
Supreme006 <a99676669@gmail.com>
2023-05-19 17:52:48 +0200

    every 10 seconds

--- updater.js
+++ updater.js
@@ -2,28 +2,16 @@ require("./index.js");
 const cron = require("node-cron")
 const { exec } = require("child_process");
 
-exec('git pull', (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
+cron.schedule('*/10 * * * * *', () => {
+    exec('git pull', (error, stdout, stderr) => {
+        if (error) {
+            console.log(`${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`${stderr}`);
+            return;
+        }
+        console.log(`${stdout}`);
+    });
 });
-
-// cron.schedule('*/10 * * * * *', () => {
-//     exec("git pull", (error, stdout, stderr) => {
-//         if (error) {
-//             console.log(`error: ${error.message}`);
-//             return;
-//         }
-//         if (stderr) {
-//             console.log(`stderr: ${stderr}`);
-//             return;
-//         }
-//         console.log(`stdout: ${stdout}`);
-//     });
-// });
