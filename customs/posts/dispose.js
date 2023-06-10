f4d108a95d7669cef3b94790e63decd3d55f3ab1
Supreme006 <supremej.test@gmail.com>
2023-06-10 12:52:44 +0200

    ea

--- customs/posts/dispose.js
+++ customs/posts/dispose.js
@@ -1,19 +1,25 @@
 const data = require("../tempFiles/posts.json");
 const fs = require("fs");
 
-const allPhotos = data.data.user.edge_owner_to_timeline_media.edges;
+let allPhotos = undefined;
 const results = []
 
-for (let i = 0; i < allPhotos.length; i++) {
-    if (i == 6) break;
-    const posts = {
-        imageUrl: allPhotos[i].node.display_url,
-        text: allPhotos[i].node.edge_media_to_caption.edges[0].node.text,
-        time: allPhotos[i].node.taken_at_timestamp,
-        link: `https://www.instagram.com/p/${allPhotos[i].node.shortcode}/`,
-        id: allPhotos[i].node.id
-    }
-    results.push(posts)
+if (data.data.user.edge_owner_to_timeline_media.edges) {
+    allPhotos = data.data.user.edge_owner_to_timeline_media.edges;
 }
 
-fs.writeFileSync("./customs/tempFiles/disposedPhotos.json", JSON.stringify(results))
+if (allPhotos) {
+    for (let i = 0; i < allPhotos.length; i++) {
+        if (i == 6) break;
+        const posts = {
+            imageUrl: allPhotos[i].node.display_url,
+            text: allPhotos[i].node.edge_media_to_caption.edges[0].node.text,
+            time: allPhotos[i].node.taken_at_timestamp,
+            link: `https://www.instagram.com/p/${allPhotos[i].node.shortcode}/`,
+            id: allPhotos[i].node.id
+        }
+        results.push(posts)
+    }
+
+    fs.writeFileSync("./customs/tempFiles/disposedPhotos.json", JSON.stringify(results))
+}
