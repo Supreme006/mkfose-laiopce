1a2749679f55651e9ff34be667de5631716da6ce
Supreme006 <supremej.test@gmail.com>
2023-06-19 22:39:52 +0200

    asd

--- public/js/addtocart.js
+++ public/js/addtocart.js
@@ -36,88 +36,28 @@ function color(element){
   }
 }
 
-
-async function red() {
-  if (!color) {
-    color = "red";
-    const element = document.getElementById("red")
-    element.classList.remove("colorCentarWite")
-    element.classList.add("colorCentarSelected")
-  } else {
-    const element = document.getElementById(color)
-    const element2 = document.getElementById("red")
-    element.classList.remove("colorCentarSelected")
-    element.classList.add("colorCentarWite")
-    element2.classList.remove("colorCentarWite")
-    element2.classList.add("colorCentarSelected")
-    color = "red"
-  }
-}
-
-async function pink() {
-  if (!color) {
-    color = "pink";
-    const element = document.getElementById("pink")
-    element.classList.remove("colorCentarWite")
-    element.classList.add("colorCentarSelected")
-  } else {
-    const element = document.getElementById(color)
-    const element2 = document.getElementById("pink")
-    element.classList.remove("colorCentarSelected")
-    element.classList.add("colorCentarWite")
-    element2.classList.remove("colorCentarWite")
-    element2.classList.add("colorCentarSelected")
-    color = "pink"
-  }
-}
-
-async function black() {
-  if (!color) {
-    color = "black";
-    const element = document.getElementById("black")
-    element.classList.remove("colorCentarWite")
-    element.classList.add("colorCentarSelected")
-  } else {
-    const element = document.getElementById(color)
-    const element2 = document.getElementById("black")
-    element.classList.remove("colorCentarSelected")
-    element.classList.add("colorCentarWite")
-    element2.classList.remove("colorCentarWite")
-    element2.classList.add("colorCentarSelected")
-    color = "black"
-  }
-}
-
-async function grey() {
-  if (!color) {
-    color = "grey";
-    const element = document.getElementById("grey")
-    element.classList.remove("colorCentarWite")
-    element.classList.add("colorCentarSelected")
-  } else {
-    const element = document.getElementById(color)
-    const element2 = document.getElementById("grey")
-    element.classList.remove("colorCentarSelected")
-    element.classList.add("colorCentarWite")
-    element2.classList.remove("colorCentarWite")
-    element2.classList.add("colorCentarSelected")
-    color = "grey"
-  }
-}
-
 async function addToCart() {
   const title = document.getElementById("title").textContent;
   // const price = document.getElementById("price").innerText;
   // const value = document.getElementById("value").innerText;
   // const s_description = document.getElementById("s_description")
+  const s = [];
+  const c = [];
+  sizes.forEach(siz => {
+    s.push(`\"${siz}\"`)
+  })
+  colors.forEach(col => {
+    s.push(`\"${col}\"`)
+  })
   $.ajax({
     url: "/addToCart",
     type: "POST",
     data: {
-      title: title
+      title: title,
+      sizes: s,
+      colors: c
     },
     success: (r) => {
-
       if (r.response == "added") {
         alert("ADDED TO CART")
       }
