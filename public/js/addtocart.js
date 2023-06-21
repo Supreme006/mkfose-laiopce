79c28dabafc0c74c2903c3afa1a494d59db60d2b
Supreme006 <supremej.test@gmail.com>
2023-06-21 13:19:36 +0200

    asdasd

--- public/js/addtocart.js
+++ public/js/addtocart.js
@@ -1,5 +1,7 @@
-const colors = [];
-const sizes = [];
+// const colors = [];
+// const sizes = [];
+let color;
+let size;
 
 function removeFromArray(arr, value) {
   var index = arr.indexOf(value);
@@ -12,27 +14,46 @@ function removeFromArray(arr, value) {
 function size(element){
   const selected_size = element.className
   const id = document.getElementById(selected_size)
-  if(sizes.includes(selected_size)){
-    removeFromArray(sizes, selected_size)
-    id.classList.remove("sizeS2")
-  } else {
-    sizes.push(selected_size)
+  // if(sizes.includes(selected_size)){
+  //   removeFromArray(sizes, selected_size)
+  //   id.classList.remove("sizeS2")
+  // } else {
+  //   sizes.push(selected_size)
+  //   id.classList.add("sizeS2")
+  // }
+  if(!size){
     id.classList.add("sizeS2")
+    size = selected_size;
+  } else {
+    const old = document.getElementById(size)
+    old.classList.remove("sizeS2")
+    id.classList.add("sizeS2");
+    size = selected_size;
   }
 }
 
 function color(element){
-
   const selected_color = element.classList[1] || element.classList[0];
-
   const id = document.getElementById(selected_color)
-  if(colors.includes(selected_color)){
-    removeFromArray(colors, selected_color)
-    id.classList.remove("colorCentarSelected")
-    id.classList.add("colorCentarWite")
-  } else {
-    colors.push(selected_color)
+  // if(colors.includes(selected_color)){
+  //   removeFromArray(colors, selected_color)
+  //   id.classList.remove("colorCentarSelected")
+  //   id.classList.add("colorCentarWite")
+  // } else {
+  //   colors.push(selected_color)
+  //   id.classList.add("colorCentarSelected")
+  // }
+  if(!color){
+    id.classList.remove("colorCentarWite")
     id.classList.add("colorCentarSelected")
+    color = selected_color;
+  } else {
+    const old = document.getElementById(size)
+    old.classList.remove("colorCentarSelected")
+    old.classList.add("colorCentarWite")
+    id.classList.remove("colorCentarWite");
+    id.classList.add("colorCentarSelected");
+    color = selected_color;
   }
 }
 
@@ -41,21 +62,23 @@ async function addToCart() {
   // const price = document.getElementById("price").innerText;
   // const value = document.getElementById("value").innerText;
   // const s_description = document.getElementById("s_description")
-  const s = [];
-  const c = [];
-  sizes.forEach(siz => {
-    s.push(`\"${siz}\"`)
-  })
-  colors.forEach(col => {
-    s.push(`\"${col}\"`)
-  })
+
+  // const s = [];
+  // const c = [];
+  // sizes.forEach(siz => {
+  //   s.push(`\"${siz}\"`)
+  // })
+  // colors.forEach(col => {
+  //   s.push(`\"${col}\"`)
+  // })
+
   $.ajax({
     url: "/addToCart",
     type: "POST",
     data: {
       title: title,
-      sizes: s,
-      colors: c
+      sizes: size,
+      colors: color
     },
     success: (r) => {
       if (r.response == "added") {
